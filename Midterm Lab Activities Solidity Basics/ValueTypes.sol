6a90c3139e18bcf00c0c7de64811f9da5a2bc20b
Andrei Bagang <98503728+Wontat@users.noreply.github.com>
2024-02-07 10:48:01 +0800

    Add files via upload

---
+++ Midterm Lab Activities Solidity Basics/ValueTypes.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ValueTypes {
+    bool public b = true;
+    uint public u = 123;
+
+    int public i =123;
+
+    int public minInt = type(int).min;
+
+    address public  addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+}
