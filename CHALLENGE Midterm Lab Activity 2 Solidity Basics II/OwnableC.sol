31e9d7c4f9dd7bdc750c920e65741b74ba87d62d
Andrei Bagang <98503728+Wontat@users.noreply.github.com>
2024-02-28 23:57:35 +0800

    OwnableC.sol

--- CHALLENGE Midterm Lab Activity 2 Solidity Basics II/OwnableC.sol
+++ CHALLENGE Midterm Lab Activity 2 Solidity Basics II/OwnableC.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 contract Ownable {
     // Declare a state variable owner of type address to store the contract owner's address
